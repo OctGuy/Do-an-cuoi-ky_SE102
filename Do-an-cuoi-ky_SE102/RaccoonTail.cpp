8d75bedb0edae089f770465fa079c2624bb537a0
Phong Nguyen <31173206+kllp031@users.noreply.github.com>
2025-04-29 15:17:20 +0700

    Added particle effect when mario tail hit Goomba, Koopa, Plant

--- Do-an-cuoi-ky_SE102/RaccoonTail.cpp
+++ Do-an-cuoi-ky_SE102/RaccoonTail.cpp
@@ -37,9 +37,14 @@ void CRaccoonTail::OnCollisionWith(LPCOLLISIONEVENT e)
 {
     //if (!IsActive()) return;
 
+    CParticle* particle = new CParticle(x, y, PARTICLE_TYPE_HIT);
+    CPlayScene* currentScene = dynamic_cast<CPlayScene*>(CGame::GetInstance()->GetCurrentScene());
+    
+
     if (dynamic_cast<CGoomba*>(e->obj))
     {
         OnCollisionWithGoomba(e);
+        currentScene->Add(particle);
     }
     else if (dynamic_cast<CQuestionBrick*>(e->obj))
     {
@@ -48,10 +53,12 @@ void CRaccoonTail::OnCollisionWith(LPCOLLISIONEVENT e)
     else if (dynamic_cast<CKoopa*>(e->obj))
     {
         OnCollisionWithKoopa(e);
+        currentScene->Add(particle);
     }
     else if (dynamic_cast<CPiranhaPlant*>(e->obj))
     {
         OnCollisionWithPiranhaPlant(e);
+        currentScene->Add(particle);
     }
 }
 
